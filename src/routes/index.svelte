a99faf3633d5f8d47cdef08b33af327932661900
JustinVx <github@justinvenbrux.nl>
2021-12-16 11:57:10 +0100

    Added Tailwind/Typography

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,7 @@
 <div class="flex justify-center">
-  <div class="w-1/2 mt-16">
-    <div class="block w-full">
-      <h1>NoteCodeSpace</h1>
-    </div>
-    <div>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</div>
-  </div>
+  <article class="prose w-1/2 mt-16 ">
+    <h1>NoteCodeSpace</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+  </article>
 </div>
 
