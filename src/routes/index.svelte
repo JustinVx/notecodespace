22c778d6a2419fc300806facbe6a6b195520146c
JustinVx <github@justinvenbrux.nl>
2021-12-16 11:42:50 +0100

    Include Tailwind

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex justify-center">
+  <div class="w-1/2 mt-16">
+    <div class="block w-full">
+      <h1>NoteCodeSpace</h1>
+    </div>
+    <div>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</div>
+  </div>
+</div>
+
