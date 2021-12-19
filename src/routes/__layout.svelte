ac878dec24f0a05efb48dafc9a35bd617ed0bd14
JustinVx <github@justinvenbrux.nl>
2021-12-19 20:18:58 +0100

    Wider header

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,7 +5,7 @@
 
 <div class="flex flex-col bg-teal-50 dark:bg-gray-800">
   <header>
-    <div class="flex flex-row justify-between max-w-4xl mx-auto lg:mt-8 lg:mb-4">
+    <div class="flex flex-row justify-between max-w-5xl mx-auto lg:mt-8 lg:mb-4">
       <h1 class="ml-6 mr-8 my-8 text-xl font-medium dark:text-gray-300"><a href="/" class="hover:text-teal-600">notecode.space</a></h1>
       <div class="mx-8 my-8"><Emojis /></div> 
     </div>
