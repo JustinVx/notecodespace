8c1a938d92ba887fa0154bc7ef1f4ed773305ce0
JustinVx <github@justinvenbrux.nl>
2021-12-19 14:45:20 +0100

    Updated index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,18 +8,29 @@
 
 <script>
   export let posts;
-  $: console.log(posts)
+
+  const formatDate = (value) => {
+		const date = new Date(value);
+		return new Intl.DateTimeFormat('en-UK', {
+			year: 'numeric',
+			month: 'long',
+			day: undefined
+		}).format(date);
+	};
 </script>
 
-<div class="prose dark:prose-dark">
+<div class="prose dark:prose-dark lg:prose-lg xl:prose-xl h-screen">
   <div>
-    <h2 class="font-medium">A wonderfull list of projects</h2>
-    <ol>
+    <p class="font-medium">Welcome to <b>notecode.page</b>. It's the website where I keep updates of the small side-projects that I work on and have worked on. Here they are:</p>
+    <div>
       {#each posts as i}
-        <li><a href="/{i.slug}">{i.title}</a>
-        </li>
+        <div><a href="/{i.slug}">{i.title}</a></div>
+        <div class="text-sm text-gray-400">Built in {formatDate(i.date)}. Spent {i.time} and {i.costs} on it.</div>
       {/each}
-    </ol>
+      </div>
   </div>
 </div>
+
+
+
 
