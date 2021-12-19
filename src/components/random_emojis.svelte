c9f8465e35f0e2a385c5a3b62bb62f2911bf7eaf
JustinVx <github@justinvenbrux.nl>
2021-12-19 14:44:57 +0100

    Created random emoji generator

---
+++ src/components/random_emojis.svelte
@@ -0,0 +1,18 @@
+<script>
+  let emojis = ['☂️','🤯','🩴','⛑','🐯','🦑','🦜','☄️','🔥','🥐','🍿','🍾','🌰','⚽️','🧘‍♂️','🥁','🛺','🚞','🛤','🏕','⛰','🧯','🧲','🔭','🚰','🪅','🇦🇶','🍇','🥒','🦭','🐡','🥽','🦸‍♂️','🧔‍♂️','👍','🧠','🙀','😮‍💨','🤤','😬','🥺','😜','🙂','🔆','❤️‍🔥','🔍','🪄']
+  let random = Math.floor(Math.random() * emojis.length);
+  let emoji1 = emojis[random]
+  let emoji2 = emojis[Math.floor(Math.random() * emojis.length)]
+  let emoji3 = emojis[Math.floor(Math.random() * emojis.length)]
+
+  function changeEmojis() {
+    emoji1 = emojis[Math.floor(Math.random() * emojis.length)]
+    emoji2 = emojis[Math.floor(Math.random() * emojis.length)]
+    emoji3 = emojis[Math.floor(Math.random() * emojis.length)]
+  }
+</script>
+
+<button on:click={changeEmojis}>
+  {@html emoji1} {@html emoji2} {@html emoji3}
+</button>
+
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,16 +1,19 @@
 <script>
 	import '../app.postcss';
+  import Emojis from '../components/random_emojis.svelte'
 </script>
 
-<div class="h-screen flex flex-col bg-teal-50 dark:bg-gray-800">
+<div class="flex flex-col bg-teal-50 dark:bg-gray-800">
   <header>
-    <div class="max-w-2xl mx-auto">
-      <h1 class="py-8 font-medium dark:text-gray-300"><a href="/" class="hover:text-teal-600">NoteCodeSpace</a></h1>
+    <div class="flex flex-row justify-between max-w-4xl mx-auto lg:mt-8 lg:mb-4">
+      <h1 class="ml-6 mr-8 my-8 text-xl font-medium dark:text-gray-300"><a href="/" class="hover:text-teal-600">notecode.space</a></h1>
+      <div class="mx-8 my-8"><Emojis /></div> 
     </div>
   </header>
   <main>
-    <div class="max-w-2xl mx-auto">
+    <div class="mx-6 max-w-2xl md:mx-auto">
       <slot />
     </div>
   </main>
+  <footer class="mb-32 mt-16"></footer>
 </div>
